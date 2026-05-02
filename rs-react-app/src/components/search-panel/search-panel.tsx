2e97502cd4804b47e6ab31c231c199eb163ecbc9
Andriy <andriibabi6@gmail.com>
2026-05-03 02:02:11 +0300

    feat: add input handler and add loading local Storage Input after Mounting Component

--- rs-react-app/src/components/search-panel/search-panel.tsx
+++ rs-react-app/src/components/search-panel/search-panel.tsx
@@ -1,16 +1,45 @@
-import { Component, type ReactNode } from "react";
-import "./search-panel.css"
+import { Component, type ChangeEvent, type ReactNode } from 'react';
+import './search-panel.css';
 
-export default class SearchPanel extends Component{
+type SearchPanelState = {
+  searchTerm: string;
+};
 
+export default class SearchPanel extends Component<object, SearchPanelState> {
+  state: SearchPanelState = {
+    searchTerm: '',
+  };
 
-    render(): ReactNode {
-        return <section className="search-panel">
-            <button className="search-button" type="button" aria-label="Search">
-                <span className="material-symbols-outlined">search</span>
-            </button>
-            <input className="search-input-field" type="search" placeholder="Search"/>
-            
-        </section>
+  componentDidMount(): void {
+    const savedSearchTerm = localStorage.getItem('searchTerm');
+
+    if (savedSearchTerm) {
+      this.setState({
+        searchTerm: savedSearchTerm,
+      });
     }
+  }
+
+  handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      searchTerm: event.target.value,
+    });
+  };
+
+  render(): ReactNode {
+    return (
+      <section className="search-panel">
+        <button className="search-button" type="button" aria-label="Search">
+          <span className="material-symbols-outlined">search</span>
+        </button>
+        <input
+          className="search-input-field"
+          type="search"
+          placeholder="Search"
+          value={this.state.searchTerm}
+          onChange={this.handleInputChange}
+        />
+      </section>
+    );
+  }
 }
