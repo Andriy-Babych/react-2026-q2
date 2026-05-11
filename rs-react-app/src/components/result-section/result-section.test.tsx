15c82793da23e55f854b7c03885347bf9293f4dc
Andriy <andriibabi6@gmail.com>
2026-05-11 19:55:42 +0300

    test: move test for an ErrorButton into error-button.test.tsx and add 1 new test

--- rs-react-app/src/components/result-section/result-section.test.tsx
+++ rs-react-app/src/components/result-section/result-section.test.tsx
@@ -27,14 +27,14 @@ test("renders No Such FOOD if array is empty", () => {
     expect(screen.getByText("No Such FOOD")).toBeInTheDocument();
 })
 
-test("renders error when click showError button", async () => {
-    const onShowError = vi.fn();
-    render(
-        <ResultSection results={[]} onShowError={onShowError}/>
-    )
+// test("renders error when click showError button", async () => {
+//     const onShowError = vi.fn();
+//     render(
+//         <ResultSection results={[]} onShowError={onShowError}/>
+//     )
 
-    const user = userEvent.setup();
-    await user.click(screen.getByRole("button"));
+//     const user = userEvent.setup();
+//     await user.click(screen.getByRole("button"));
 
-    expect(onShowError).toHaveBeenCalled();
-})
+//     expect(onShowError).toHaveBeenCalled();
+// })
