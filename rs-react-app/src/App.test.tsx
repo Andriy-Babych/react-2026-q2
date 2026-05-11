fffc5dd616f80900d442af06ed1d86f245b26cf0
Andriy <andriibabi6@gmail.com>
2026-05-12 00:44:44 +0300

    test: add tests for App.tsx, moking requests

---
+++ rs-react-app/src/App.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react';
+import { test, expect, vi } from 'vitest';
+import App from './App';
+import userEvent from '@testing-library/user-event';
+
+test('shows loading while fetching data', () => {
+  vi.stubGlobal(
+    'fetch',
+    vi.fn(() => new Promise(() => {}))
+  );
+
+  render(<App />);
+
+  expect(screen.getByText('Loading...')).toBeInTheDocument();
+});
+
+test('', async () => {
+  vi.stubGlobal(
+    'fetch',
+    vi.fn(() =>
+      Promise.resolve({
+        ok: true,
+        json: async () => ({
+            foods: [
+            {
+              uid: 'FOMA0000264633',
+              name: 'Abalone',
+              earthlyOrigin: 'Earth',
+            },
+          ],
+        }),
+      })
+    )
+  );
+
+  render(<App />);
+
+  expect(await screen.findByText("Abalone")).toBeInTheDocument();
+  expect(screen.getByText("FOMA0000264633")).toBeInTheDocument();
+  expect(screen.getByText("Origin: Earth")).toBeInTheDocument();
+});
--- rs-react-app/src/components/result-section/result-section.test.tsx
+++ rs-react-app/src/components/result-section/result-section.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import { test, expect, vi } from 'vitest';
-import userEvent from '@testing-library/user-event';
 import ResultSection from './result-section';
 
 test('renders item id name and description', () => {
