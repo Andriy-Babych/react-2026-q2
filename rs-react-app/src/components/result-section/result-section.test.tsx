b1de51952e932c52a4c45f4faa3ba137e2ac5f90
Andriy <andriibabi6@gmail.com>
2026-05-11 19:31:11 +0300

    test: add tests for resultSection

--- rs-react-app/src/components/result-section/result-section.test.tsx
+++ rs-react-app/src/components/result-section/result-section.test.tsx
@@ -17,3 +17,24 @@ test('renders item id name and description', () => {
   expect(screen.getByText("Abalone")).toBeInTheDocument();
   expect(screen.getByText("Origin: true")).toBeInTheDocument();
 });
+
+
+test("renders No Such FOOD if array is empty", () => {
+    render(
+        <ResultSection results={[]} onShowError={vi.fn()}/>
+    )
+
+    expect(screen.getByText("No Such FOOD")).toBeInTheDocument();
+})
+
+test("renders error when click showError button", async () => {
+    const onShowError = vi.fn();
+    render(
+        <ResultSection results={[]} onShowError={onShowError}/>
+    )
+
+    const user = userEvent.setup();
+    await user.click(screen.getByRole("button"));
+
+    expect(onShowError).toHaveBeenCalled();
+})
