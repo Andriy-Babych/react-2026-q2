c0ccfbcc167b58add72f7570d862187e35fbacfa
Andriy <andriibabi6@gmail.com>
2026-05-11 15:58:07 +0300

    test: add SearchPanel interactions tests

--- rs-react-app/src/components/search-panel/search-panel.test.tsx
+++ rs-react-app/src/components/search-panel/search-panel.test.tsx
@@ -1,11 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import { test, expect, vi, afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
+import { test, expect, vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
 import SearchPanel from './search-panel';
-
-afterEach(() => {
-    cleanup();
-})
 
 test('Renders search panel', () => {
   render(
@@ -19,14 +15,50 @@ test('Renders search panel', () => {
   expect(screen.getByRole('searchbox')).toBeInTheDocument();
 });
 
-test("displays search term value", () => {
-    render(
-        <SearchPanel
-          searchTerm="try"
-          onSearchTermChange={vi.fn()}
-          onSearch={vi.fn()}
-        />
-      );
+test('displays search term value', () => {
+  render(
+    <SearchPanel
+      searchTerm="try"
+      onSearchTermChange={vi.fn()}
+      onSearch={vi.fn()}
+    />
+  );
 
-    expect(screen.getByRole('searchbox')).toHaveValue("try")
-})
+  expect(screen.getByRole('searchbox')).toHaveValue('try');
+});
+
+test('onSearchTermChange callback called', async () => {
+  const handleSearchTermChange = vi.fn();
+  render(
+    <SearchPanel
+      searchTerm=""
+      onSearchTermChange={handleSearchTermChange}
+      onSearch={vi.fn()}
+    />
+  );
+
+  const user = userEvent.setup();
+  const input = screen.getByRole('searchbox');
+
+  await user.type(input, 'pizza');
+
+  expect(handleSearchTermChange).toHaveBeenCalledTimes(5);
+});
+
+test('search button click', async () => {
+  const onSearch = vi.fn();
+  render(
+    <SearchPanel
+      searchTerm=""
+      onSearchTermChange={vi.fn()}
+      onSearch={onSearch}
+    />
+  );
+
+  const user = userEvent.setup();
+  const searchButton = screen.getByRole('button', { name: 'Search' });
+
+  await user.click(searchButton);
+
+  expect(onSearch).toHaveBeenCalled();
+});
