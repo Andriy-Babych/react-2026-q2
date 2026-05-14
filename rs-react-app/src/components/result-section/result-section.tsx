05d08ad37c95e9a0982618513fb5985407b9703c
Andriy <andriibabi6@gmail.com>
2026-05-14 18:07:12 +0300

    refactor: turn ResultSection class component into functional component

--- rs-react-app/src/components/result-section/result-section.tsx
+++ rs-react-app/src/components/result-section/result-section.tsx
@@ -1,4 +1,3 @@
-import { Component, type ReactNode } from 'react';
 import './result-section.css';
 import ErrorButton from '../error-button/error-button';
 
@@ -13,26 +12,27 @@ type ResultSectionProps = {
   onShowError: () => void;
 };
 
-export default class ResultSection extends Component<ResultSectionProps> {
-  render(): ReactNode {
-    return (
-      <section className="result-section">
-        {this.props.results.length === 0 ? (
-          <div className="searchedItemNotFound">No Such FOOD</div>
-        ) : (
-          this.props.results.map((resultElement) => (
-            <div className="result-item" key={resultElement.id}>
-              <div className="result-item_id">{resultElement.id}</div>
-              <div className="result-item_name">{resultElement.name}</div>
-              <div className="result-item_description">
-                {resultElement.description}
-              </div>
+export default function ResultSection({
+  results,
+  onShowError,
+}: ResultSectionProps) {
+  return (
+    <section className="result-section">
+      {results.length === 0 ? (
+        <div className="searchedItemNotFound">No Such FOOD</div>
+      ) : (
+        results.map((resultElement) => (
+          <div className="result-item" key={resultElement.id}>
+            <div className="result-item_id">{resultElement.id}</div>
+            <div className="result-item_name">{resultElement.name}</div>
+            <div className="result-item_description">
+              {resultElement.description}
             </div>
-          ))
-        )}
+          </div>
+        ))
+      )}
 
-        <ErrorButton onShowError={this.props.onShowError} />
-      </section>
-    );
-  }
+      <ErrorButton onShowError={onShowError} />
+    </section>
+  );
 }
