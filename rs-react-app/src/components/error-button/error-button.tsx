51c278a172c1e6e710bf30edc83f59a398565189
Andriy <andriibabi6@gmail.com>
2026-05-14 18:08:40 +0300

    refactor: turn ErrorButton class component into functional component

--- rs-react-app/src/components/error-button/error-button.tsx
+++ rs-react-app/src/components/error-button/error-button.tsx
@@ -1,21 +1,18 @@
-import { Component, type ReactNode } from 'react';
 import './error-button.css';
 
 type ErrorButtonProps = {
   onShowError: () => void;
 };
 
-export default class ErrorButton extends Component<ErrorButtonProps> {
-  render(): ReactNode {
-    return (
-      <button
-        onClick={this.props.onShowError}
-        className="error-button"
-        type="button"
-        aria-label="error-button"
-      >
-        Simulate Error!
-      </button>
-    );
-  }
+export default function ErrorButton({ onShowError }: ErrorButtonProps) {
+  return (
+    <button
+      onClick={onShowError}
+      className="error-button"
+      type="button"
+      aria-label="error-button"
+    >
+      Simulate Error!
+    </button>
+  );
 }
