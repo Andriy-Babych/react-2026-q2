a7d26c8f08bb5d61b4d88ccde3209757a50bbfe0
Andriy <andriibabi6@gmail.com>
2026-05-18 10:38:18 +0300

    feat: add routing logic

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
+
+import { Route, Routes } from 'react-router-dom';
+
 import HomePage from './pages/home-page/home-page';
+import AboutPage from './pages/about-page/about-page';
+import NotFoundPage from './pages/404 page/not-found-page';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import ErrorBoundary from './components/error-boundary/error-boundary.tsx';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ErrorBoundary>
   </StrictMode>
 );
