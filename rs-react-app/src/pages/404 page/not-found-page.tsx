79634fde3ee03e1d9033b711f2fa8fcaf2e92fbc
Andriy <andriibabi6@gmail.com>
2026-05-18 14:03:34 +0300

    feat: style about page

--- rs-react-app/src/pages/404 page/not-found-page.tsx
+++ rs-react-app/src/pages/404 page/not-found-page.tsx
@@ -5,8 +5,8 @@ export default function NotFoundPage() {
   return (
     <div className="notFoundPage">
       <h1>404</h1>
-      <p>Opsss...<br />The page you are looking for does not exist.</p>
-      <button className='back_home-button'><Link to="/"> Go Back</Link></button>
+      <p>Oops...<br />The page you are looking for does not exist.</p>
+      <Link to="/" className='back_home-button'> Go Back</Link>
     </div>
   );
 }
--- rs-react-app/src/pages/about-page/about-page.tsx
+++ rs-react-app/src/pages/about-page/about-page.tsx
@@ -1,8 +1,23 @@
+import './about-page.css';
+
 export default function AboutPage() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the about page of our React application.</p>
+    <div className="about_page-container">
+
+      <h1>About This Project</h1>
+
+      <h3 className='author'>Author: <span>Andriy Babych</span></h3>
+
+      <p className='description'>This application was created as part of the RS School React course.<br />
+        The project demonstrates React Hooks, React Router, API integration,
+        pagination, and modern SPA architecture.
+      </p>
+
+      <p className='description'>The application allows users to search Star Trek food items using the STAPI API and explore results in a user-friendly interface.
+      </p>
+
+      <p className='description'><a className='course-link' href="https://app.rs.school/course/stats?course=react-2026-q2" target="_blank" rel="noopener noreferrer">RS School React Course </a>
+      </p>
     </div>
   );
 }
