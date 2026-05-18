ec1dc2fb97862426ca30730d1e1a51584343f712
Andriy <andriibabi6@gmail.com>
2026-05-18 13:05:36 +0300

    feat: style 404 page

--- rs-react-app/src/pages/404 page/not-found-page.tsx
+++ rs-react-app/src/pages/404 page/not-found-page.tsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
+import './not-found-page.css';
+
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div className="notFoundPage">
+      <h1>404</h1>
+      <p>Opsss...<br />The page you are looking for does not exist.</p>
+      <button className='back_home-button'><Link to="/"> Go Back</Link></button>
     </div>
   );
 }
